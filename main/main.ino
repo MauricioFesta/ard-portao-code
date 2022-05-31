d90f7b5a3843b352cd7969caa06e10b1cb5d712d
mauricio <mauricio.festa@icloud.com>
2022-05-30 22:50:13 -0300

    okko

--- main/main.ino
+++ main/main.ino
@@ -5,8 +5,7 @@ SoftwareSerial esp8266(10,11);
 #define LED 8                          
 #define SERVER true
 
-long int time_before = millis() + 4000; 
-int contador = 0;
+long int contador = 0;
 void setup()
 
 {
@@ -21,56 +20,74 @@ void setup()
 void loop()                                                         
 {
 
+ 
+  if((contador) > 1000000){
 
-  //if((time_before+6000) > millis()){
 
-      //Serial.print("Teste de connn.... ");
-      //sendData("AT+CWJAP?\r\n", 2000, DEBUG);
-       //esp8266.print("AT+CWJAP?\r\n");
+     sendData("AT+CWJAP?\r\n", 2000, DEBUG);
+     sendData("AT+CIPSERVER=1,76\r\n", 1000, SERVER);  
+         
+      
+  }
 
-       //time_before = millis(); 
-    
-  //}
-  
-    
-  
+
+
+
   if(esp8266.available())                                           
- {    
-   
-      if(esp8266.find("+IPD,")){
+ {   
+
     
+      if(esp8266.find("+IPD,")){
+
+        digitalWrite(LED, HIGH);
+        delay(1000);
+        digitalWrite(LED, LOW);
+
+       
         int connectionId = esp8266.read()-48;
        
-        String response = "";
-           contador = 0;
+        //String response = "";
            
-         while(esp8266.available())                                      
-          {
-            char c = esp8266.read();                                     
-            response+=c;    
+         //while(esp8266.available())                                      
+          //{
+            //char c = esp8266.read();                                     
+            //response+=c;    
                                                        
-          }  
+          //}  
+
+         // Serial.print("Response");
+          //Serial.print("\n");
+          //Serial.print(response);
+          //Serial.print("\n");
+
+              
+      
           
-           if(response.indexOf("/zdbbyrs") != -1){
+          // if(response.indexOf("/FdGAKx3jeLws2hw") != -1){
     
-            Serial.print("Entrou no IF");
+             // Serial.print("Entrou no IF");
+      
+             
+              //digitalWrite(LED, HIGH);
+              //delay(1200);
+             // digitalWrite(LED, LOW);
+      
+              //delay(1000);
     
-           
-            digitalWrite(LED, HIGH);
-            delay(1000);
-            digitalWrite(LED, LOW);
-    
-             String closeCommand = "AT+CIPCLOSE="; 
-            closeCommand+=connectionId; 
-            closeCommand+="\r\n"; 
-            esp8266.print(closeCommand);
-            delay(1000);
-    
-          }
+        // }
+
+
+          
+               String closeCommand = "AT+CIPCLOSE="; 
+              closeCommand+=connectionId; 
+              closeCommand+="\r\n"; 
+              esp8266.print(closeCommand);
     
            
         }
     }
+
+      contador+= 1;
 
 }
 
@@ -91,13 +108,15 @@ String sendData(String command, const int timeout, boolean debug)
     }    
 
 
-  if(response.indexOf("ERROR") != -1){
-
+  if(response.indexOf("ERROR") != -1 || response.indexOf("No AP") != -1){
+   
     init_config();
   }
     
    
       Serial.print(response);
+      contador = 0;
+      
    
        
     return response;                                                  
@@ -107,15 +126,15 @@ void init_config()
 {
 
   Serial.print("Config Call..");
-  sendData("AT+RST\r\n", 2000, DEBUG);                                                  
-  sendData("AT+CWJAP=\"AP-02_2GHz\",\"riceferteste\"\r\n", 2000, DEBUG);        
+  sendData("AT+RST\r\n", 2000, DEBUG);                                                        
+  sendData("AT+CWJAP=\"AP-02_2GHz\",\"riceferteste\"\r\n", 4000, DEBUG);      
   delay (3000);
-  sendData("AT+CWMODE=1\r\n", 1500, DEBUG);                                             
-  delay (1500);
-  sendData("AT+CIFSR\r\n", 1500, DEBUG);                                             
-  delay (1500);
-  sendData("AT+CIPMUX=1\r\n", 1500, DEBUG);                                             
-  delay (1500);
-  sendData("AT+CIPSERVER=1,76\r\n", 1500, SERVER);                                     
+  sendData("AT+CWMODE=1\r\n", 1000, DEBUG);                                             
+  delay (3000);
+  sendData("AT+CIFSR\r\n", 1000, DEBUG);                                             
+  delay (3000);
+  sendData("AT+CIPMUX=1\r\n", 1000, DEBUG);                                             
+  delay (3000);
+  sendData("AT+CIPSERVER=1,76\r\n", 1000, SERVER);                                     
 
 }
